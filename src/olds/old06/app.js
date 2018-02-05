e5871093548f5d85dd481a894ba93a200d1ae285
elTorres <fernando.torres@gmail.com>
2018-02-05 19:14:22 +0100

    Monster Game

---
+++ src/olds/old06/app.js
@@ -0,0 +1,10 @@
+var App = new Vue({
+    el: '#app',
+    data: {
+        ingredients: ['meat','fruit', 'cookies'],
+        persons: [
+            {name: 'Max', age: 27, color: 'red'},
+            {name: 'Fernando', age: 'unknow', color: 'blue'}
+        ]
+    }
+});
