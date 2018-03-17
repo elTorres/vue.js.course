393cdd0c7f2710a1c1321bccd26a4ae7b00e5733
elTorres <fernando.torres@gmail.com>
2018-03-17 13:21:18 +0100

    Compartir datos entre instancias de un componente

--- old/src/app.js
+++ old/src/app.js
@@ -1,10 +1,15 @@
+var data = {status: 'Critical'};
+
 Vue.component('my-component', {
     data: function() {
-        return {
-            status: 'Critical'
-        }
+        return data;
     },
-    template: '<p>Server status: {{status}}</p>'
+    template: '<p>Server status: {{status}} <button @click="changeStatus">Change value</button></p>',
+    methods: {
+        changeStatus: function() {
+            this.status = 'Normal';
+        }
+    }
 })
 var vm1 = new Vue({
     el: "#app"
