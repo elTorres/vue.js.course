d225d443920ddeb6e540ffa7eea34c4070e91d18
elTorres <fernando.torres@gmail.com>
2018-03-17 13:30:33 +0100

    Declaración de componentes global vs local

--- old/src/app.js
+++ old/src/app.js
@@ -1,8 +1,6 @@
-var data = {status: 'Critical'};
-
 Vue.component('my-component', {
     data: function() {
-        return data;
+        return {status: 'Critical'};
     },
     template: '<p>Server status: {{status}} <button @click="changeStatus">Change value</button></p>',
     methods: {
@@ -10,7 +8,25 @@ Vue.component('my-component', {
             this.status = 'Normal';
         }
     }
-})
+});
+
+var myComponent2 = {
+    data: function() {
+        return {status: 'Critical'};
+    },
+    template: '<p>Server2 status: {{status}} <button @click="changeStatus">Change value</button></p>',
+    methods: {
+        changeStatus: function() {
+            this.status = 'Normal';
+        }
+    }
+};
 var vm1 = new Vue({
     el: "#app"
+});
+var vm2 = new Vue({
+    el: "#app2",
+    components: {
+        "my-component": myComponent2
+    }
 });
