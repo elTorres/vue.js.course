8573181728b244a0c84d71bd8de1025d13f8fa25
elTorres <fernando.torres@gmail.com>
2018-03-29 10:08:04 +0200

    Se modulariza el componente

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <template>
-  <app-servers></app-servers>
+    <div class="container">
+        <div class="row">
+            <app-header/>
+        </div>
+        <hr>
+        <div class="row">
+            <app-server-list/>
+            <app-server-detail/>
+        </div>
+        <hr>
+        <div class="row">
+            <app-footer/>
+        </div>
+    </div>
 </template>
 
 <script>
+    import Header from './components/shared/Header.vue';
+    import Footer from './components/shared/Footer.vue';
+    import ServerList from './components/server/ServerList.vue';
+    import ServerDetail from './components/server/ServerDetail.vue';
+    export default {
+        components: {
+            'app-header': Header,
+            'app-server-list': ServerList,
+            'app-server-detail': ServerDetail,
+            'app-footer': Footer
+        }
+    }
 </script>
-
-<style>
-</style>
